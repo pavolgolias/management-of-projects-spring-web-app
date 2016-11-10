8ceaab80aca68208382c31693a1074a194cee8e7
Patrik Malý <maly.patrik1@gmail.com>
2016-11-10 17:59:49 +0100

    fixed naming sign in/up

--- src/main/webapp/app/js/auth/signIn.js
+++ src/main/webapp/app/js/auth/signIn.js
@@ -1,49 +1,62 @@
 /**
- * Created by Patrik on 10/11/2016.
+ * Created by Patrik on 09/11/2016.
  */
 
-function signIn(){
-    var email = $("#exampleInputEmail1").val();
-    var name = $("#exampleInputName").val();
-    var surname = $("#exampleInputSurname").val();
-    var passwd = $("#exampleInputPassword1").val();
-    var passwdAgain = $("#exampleInputPassword2").val();
 
-    if(email == '' || name == '' || surname == '' || passwd == '' || passwdAgain == ''){
-        //TODO Show error dialog instead of alert
-        alert("Fields cannot be empty");
+function checkIsSignedIn(){
+    var token = localStorage.getItem("token");
+    if(token == undefined || token == '')
         return;
-    }
-
-    if(passwd !== passwdAgain){
-        //TODO Show error dialog instead of alert
-        alert("Passwords are not the same.");
-        return;
-    }
-    if(passwd.length < 6 || ! /\d/.test(passwd)){
-        //TODO Show error dialog instead of alert
-        alert("Password must have at least 6 characters and must contain at least one digit.");
-        return;
-    }
 
     $.ajax({
-        url:"/api/accounts",
+        url:"/api/accounts/me",
+        type:"GET",
+        beforeSend : function(xhr) {
+                xhr.setRequestHeader("Authorization", "Bearer " +  token);
+        },
+        success: function(data){
+            console.log(data)
+            localStorage.setItem("token", token);
+            localStorage.setItem("account",JSON.stringify(data.data));
+            window.location.replace("projects.html");
+        },
+        error: function(){}
+    })
+}
+
+function signIn(){
+  var email =  $("#exampleInputEmail1").val();
+  var password = $("#exampleInputPassword1").val();
+
+    if(email == '' || password== ''){
+        alert("Email or password cannot be empty");
+        return;
+    }else{
+        getToken(email,password);
+    }
+}
+
+function getToken(email,pass){
+
+    $.ajax({
+        url:"/api/auth/attempt",
         type:"POST",
         data:JSON.stringify({
             email: email,
-            firstName: name,
-            lastName : surname,
-            password : passwd,
-            repeatPassword : passwdAgain
+            password: pass
         }),
         contentType:"application/json; charset=utf-8",
         success: function(data){
-            window.location.replace("index.html");
+                console.log(data)
+                localStorage.setItem("token", data.data.token);
+                localStorage.setItem("account",JSON.stringify(data.data.account));
+                window.location.replace("projects.html");
         },
         error: function(xhr){
-            alert("Unable to sign in! "+xhr.status);
+            alert("Unable to log in! "+xhr.status);
         }
 
 
     })
 }
+
--- src/main/webapp/app/js/auth/signUp.js
+++ src/main/webapp/app/js/auth/signUp.js
@@ -1,64 +1,49 @@
 /**
- * Created by Patrik on 09/11/2016.
+ * Created by Patrik on 10/11/2016.
  */
 
-
-function checkIsSignedUp(){
-    var token = localStorage.getItem("token");
-    if(token == undefined || token == '')
-        return;
-
-    $.ajax({
-        url:"/api/accounts/me",
-        type:"GET",
-        beforeSend : function(xhr) {
-                xhr.setRequestHeader("Authorization", "Bearer " +  token);
-        }
-        ,
-        success: function(data){
-            console.log(data)
-            localStorage.setItem("token", token);
-            localStorage.setItem("account",JSON.stringify(data.data));
-            window.location.replace("projects.html");
-        },
-        error: function(){}
-
-    })
-}
-
 function signUp(){
-  var email =  $("#exampleInputEmail1").val();
-  var password = $("#exampleInputPassword1").val();
+    var email = $("#exampleInputEmail1").val();
+    var name = $("#exampleInputName").val();
+    var surname = $("#exampleInputSurname").val();
+    var passwd = $("#exampleInputPassword1").val();
+    var passwdAgain = $("#exampleInputPassword2").val();
 
-    if(email == '' || password== ''){
-        alert("Email or password cannot be empty");
+    if(email == '' || name == '' || surname == '' || passwd == '' || passwdAgain == ''){
+        //TODO Show error dialog instead of alert
+        alert("Fields cannot be empty");
         return;
-    }else{
-        getToken(email,password);
     }
-}
 
-function getToken(email,pass){
+    if(passwd !== passwdAgain){
+        //TODO Show error dialog instead of alert
+        alert("Passwords are not the same.");
+        return;
+    }
+    if(passwd.length < 6 || ! /\d/.test(passwd)){
+        //TODO Show error dialog instead of alert
+        alert("Password must have at least 6 characters and must contain at least one digit.");
+        return;
+    }
 
     $.ajax({
-        url:"/api/auth/attempt",
+        url:"/api/accounts",
         type:"POST",
         data:JSON.stringify({
             email: email,
-            password: pass
+            firstName: name,
+            lastName : surname,
+            password : passwd,
+            repeatPassword : passwdAgain
         }),
         contentType:"application/json; charset=utf-8",
         success: function(data){
-                console.log(data)
-                localStorage.setItem("token", data.data.token);
-                localStorage.setItem("account",JSON.stringify(data.data.account));
-                window.location.replace("projects.html");
+            window.location.replace("index.html");
         },
         error: function(xhr){
-            alert("Unable to log in! "+xhr.status);
+            alert("Unable to sign in! "+xhr.status);
         }
 
 
     })
 }
-
