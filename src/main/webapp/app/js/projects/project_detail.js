3af96aaf6528200f2d685e0a8aa0bfb95671a506
Patrik Malý <maly.patrik1@gmail.com>
2016-11-15 21:10:24 +0100

    created JS for project_detail..... The page is alive, but there are only dummy data about the users

---
+++ src/main/webapp/app/js/projects/project_detail.js
@@ -0,0 +1,57 @@
+/**
+ * Created by Patrik on 15/11/2016.
+ */
+$( document ).ready(function() {
+    var projectId = getParameterByName("id",window.location.href);
+    if(projectId != null){
+        getProjectDetail(projectId);
+    }
+
+});
+
+function getProjectDetail(projectId){
+
+    return $.ajax({
+        url: "/api/projects/"+projectId,
+        type: "GET",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", "Bearer " + localStorage.getItem("token"));
+        },
+        success: function (data) {
+            displayProject(data.data);
+        },
+        error: function (xhr) {
+            if(xhr.status == 401){
+                window.location.replace("index.html");
+            }else{
+                showMessage("Error "+xhr.status+"! Unable to load projects!")
+            }
+        }
+    });
+}
+
+function displayProject(jsonResult){
+    console.log(jsonResult);
+    $("#projectName").text(jsonResult.name);
+    $("#projectId").text("#"+jsonResult.projectId);
+    updateEditLink(jsonResult.projectId);
+    $("#projectDescription").text(jsonResult.description);
+    $("#updateDate").text((new Date(jsonResult.updatedAt)).toLocaleString());
+
+
+}
+
+function updateEditLink(projectId){
+    $("#editTaskLink").attr("href","project_edit.html?id="+projectId);
+}
+function getParameterByName(name, url) {
+    if (!url) {
+        url = window.location.href;
+    }
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
