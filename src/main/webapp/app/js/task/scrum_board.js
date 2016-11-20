2871d62083fbd23dd449f9e360ab684a6c87d4d0
Juraj Vraniak <juvraniak@gmail.com>
2016-11-20 22:54:12 +0100

    minor fixes in tasks part and start work on scrum board

---
+++ src/main/webapp/app/js/task/scrum_board.js
@@ -0,0 +1,49 @@
+/**
+ * Created by juraj on 20.11.2016.
+ */
+
+var projectId;
+
+$( document ).ready(function() {
+    projectId = getUrlParameter("id");
+    getAllTasks();
+});
+
+function getAllTasks() {
+    return $.ajax({
+        url: "/api/projects/"+projectId+"/tasks",
+        type: "GET",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("Authorization", "Bearer " + localStorage.getItem("token"));
+        },
+        success: function (data) {
+            displayTasks(data);
+        },
+        error: function (xhr) {
+            if(xhr.status == 401){
+                window.location.replace("index.html");
+            }else{
+                showMessage("Error "+xhr.status+"! Unable to load projects!")
+            }
+        }
+    });
+}
+
+function displayTasks(data) {
+    console.log(data);
+}
+
+function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+}
