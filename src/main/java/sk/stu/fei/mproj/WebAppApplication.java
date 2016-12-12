307ae3a926527aa71d1d99a42665764243250414
Andrej Kosar <andrejksr@gmail.com>
2016-12-12 14:29:26 +0100

    Changed config for deployment

--- src/main/java/sk/stu/fei/mproj/WebAppApplication.java
+++ src/main/java/sk/stu/fei/mproj/WebAppApplication.java
@@ -6,21 +6,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-//import org.springframework.boot.builder.SpringApplicationBuilder;
-//import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class WebAppApplication
-//        extends SpringBootServletInitializer
+        extends SpringBootServletInitializer
 {
     public static void main(String[] args) {
         SpringApplication.run(WebAppApplication.class, args);
     }
 
-//    @Override
-//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//        return application.sources(WebAppApplication.class);
-//    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(WebAppApplication.class);
+    }
 
     @Bean
     public MapperFactory mapperFactory() {
